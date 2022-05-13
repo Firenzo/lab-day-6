9fb744b66dd6cb35a85283aa126cb8dd58db4f6c
fjorden <firenzo.jorden@capgemini.com>
2022-05-13 10:28:48 +0200

    basic configuration

---
+++ layouts/default.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+      <h1>Dit is de default layout</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+  head () {
+    return {
+      title: 'lab-day-6',
+    }
+  },
+
+  data: () => ({
+
+  }),
+
+  computed: {
+
+  },
+
+  mounted () {
+
+  },
+
+  methods: {
+
+  }
+}
+
+</script>
+
+<style lang="scss">
+@use 'styles/main' as *;
+  h1{
+      font-family: sans-serif ;
+  }
+</style>
