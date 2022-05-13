cd17b6e9b49ac2d7d421a2a02fa35d03e2ff6185
fjorden <firenzo.jorden@capgemini.com>
2022-05-13 14:24:27 +0200

    changed pasje size

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="char">
+    <div class="image">
+      <img src="~/assets/capfinder-logo.png">
+    </div>
     <h1 class="char__title">Choose your character</h1>
     <img
       class="char__pic"
       :src="
-        require(`~/assets/images/characters/${characters[currentPerson].name}.png`)
+        require(`~/assets/images/characters/${$store.state.characters[currentPerson].name}.png`)
       "
     />
-    <h2 class="char__name">{{ characters[currentPerson].name }}</h2>
+    <h2 class="char__name">{{ $store.state.characters[currentPerson].name }}</h2>
     <div class="char__button-container">
       <button class="char__button-prev" @click="prevPerson()"></button>
       <NuxtLink class="char__button-start" to="ingang">Start</NuxtLink>
@@ -26,43 +29,6 @@ export default {
 
   data: () => ({
     currentPerson: 0,
-    characters: [
-      { name: "Amir", gender: "M" },
-      { name: "Anna", gender: "F" },
-      { name: "Anneke", gender: "F" },
-      { name: "Archana", gender: "F" },
-      { name: "Arjan", gender: "M" },
-      { name: "Bart", gender: "M" },
-      { name: "Caroline", gender: "F" },
-      { name: "Daniel", gender: "M" },
-      { name: "Dennis", gender: "M" },
-      { name: "Eelco", gender: "M" },
-      { name: "Firenzo", gender: "M" },
-      { name: "Florian", gender: "M" },
-      { name: "Freek", gender: "M" },
-      { name: "Harald", gender: "M" },
-      { name: "Jessica", gender: "F" },
-      { name: "Jurre", gender: "M" },
-      { name: "Karim", gender: "M" },
-      { name: "Ken", gender: "M" },
-      { name: "Maarten", gender: "M" },
-      { name: "Maria", gender: "F" },
-      { name: "Mohamed", gender: "M" },
-      { name: "Muhammad", gender: "M" },
-      { name: "Pim", gender: "M" },
-      { name: "Rasheed", gender: "M" },
-      { name: "Redmar", gender: "M" },
-      { name: "Rianne", gender: "F" },
-      { name: "Rik", gender: "M" },
-      { name: "Ryan", gender: "M" },
-      { name: "Stijn", gender: "M" },
-      { name: "Thijs", gender: "M" },
-      { name: "Tijmen", gender: "M" },
-      { name: "Ryan", gender: "M" },
-      { name: "WeShi", gender: "F" },
-      { name: "Yingdi", gender: "M" },
-      { name: "Yonas", gender: "M" },
-    ],
   }),
 
   computed: {},
@@ -72,13 +38,13 @@ export default {
   methods: {
     prevPerson() {
       if (this.currentPerson == 0) {
-        this.currentPerson = this.characters.length - 1;
+        this.currentPerson = this.$store.state.characters.length - 1;
       } else {
         this.currentPerson -= 1;
       }
     },
     nextPerson() {
-      if (this.currentPerson == this.characters.length - 1) {
+      if (this.currentPerson == this.$store.state.characters.length - 1) {
         this.currentPerson = 0;
       } else {
         this.currentPerson += 1;
@@ -89,8 +55,19 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+div.image{
+  display:flex;
+  justify-content: center;
+  padding-top:30px;
+
+  img {
+    display:block;
+    width: 300px;
+  }
+}
 .char {
-  background-color: #0070ad;
+  background-color: #6ac3f4;
   font-family: Arial, Helvetica, sans-serif;
 
   &__title {
@@ -125,28 +102,35 @@ export default {
     transform: translate(0px, -300px);
     width: 0;
     height: 0;
-    border-top: 50px solid #0070ad;
+    border-top: 50px solid #6ac3f4;
     border-right: 100px solid red;
-    border-bottom: 50px solid #0070ad;
+    border-bottom: 50px solid #6ac3f4;
   }
   &__button-start {
-    color: black;
+    color: white;
     text-decoration: none;
     width: 200px;
     text-align: center;
-    background-color: #f1f1f1;
+    background-color: #0070ae;
     margin: 10px;
     padding: 20px;
     font-size: 30px;
     display: block;
+    border-radius:10px;
+    box-shadow: 0 0 12px rgba(255,255,255,0.5);
+    &:hover {
+      box-shadow: 0 0 0px rgb(80,80,80,0.0);
+      background-color: #630e9e;
+      color:white;
+    }
   }
   &__button-next {
     transform: translate(0px, -300px);
     width: 0;
     height: 0;
-    border-top: 50px solid #0070ad;
+    border-top: 50px solid #6ac3f4;
     border-left: 100px solid red;
-    border-bottom: 50px solid #0070ad;
+    border-bottom: 50px solid #6ac3f4;
   }
 }
 </style>
