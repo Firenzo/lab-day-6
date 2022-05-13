e88afd9c7e2972b3d88b1a161a08172c688a2338
CORP\MSULIMAN <muhammad.refae@live.com>
2022-05-13 14:30:26 +0200

    make lift page and click on furst floor takes you to kantine

---
+++ pages/lift.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="overlay">
+      <NuxtLink class="button1" to="kantine"></NuxtLink>
+      <NuxtLink class="button2" to="2nd-floor"></NuxtLink>
+      <NuxtLink class="button3" to="3rd-floor"></NuxtLink>
+      <NuxtLink class="button4" to="4th-floor"></NuxtLink>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "LiftPage",
+};
+</script>
+
+<style lang="scss">
+@use "styles/main" as *;
+.overlay {
+  background: url("../assets/pictures/lift-1.jpg") no-repeat;
+  width: 1500px;
+  height: 1125px;
+  z-index: 10;
+  position: absolute;
+  font-family: sans-serif;
+}
+.button1 {
+  position: relative;
+    top: 630px;
+    left: 510px;
+    display: block;
+    background-color: transparent;
+    width: 50px;
+    height: 80px;
+    border: 4px solid #630e9e;
+
+    &:hover {
+      border-color: #0070ae;
+    }
+}
+.button2 {
+  position: relative;
+    top: 440px;
+    left: 510px;
+    display: block;
+    background-color: transparent;
+    width: 50px;
+    height: 80px;
+    border: 4px solid #630e9e;
+
+    &:hover {
+      border-color: #0070ae;
+    }
+}
+.button3 {
+  position: relative;
+    top: 250px;
+    left: 510px;
+    display: block;
+    background-color: transparent;
+    width: 50px;
+    height: 80px;
+    border: 4px solid #630e9e;
+
+    &:hover {
+      border-color: #0070ae;
+    }
+}
+.button4 {
+  position: relative;
+    top: 30px;
+    left: 510px;
+    display: block;
+    background-color: transparent;
+    width: 50px;
+    height: 80px;
+    border: 4px solid #630e9e;
+
+    &:hover {
+      border-color: #0070ae;
+    }
+}
+
+</style>
